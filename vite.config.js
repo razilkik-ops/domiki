c51607dab0007861ae9af3eab139602fca28141f
pasha <razilkik@mail.ru>
2026-08-24 17:49:21 +0300

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'node:path';
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/domiki/' : '/',
   build: {
     rollupOptions: {
       input: {
